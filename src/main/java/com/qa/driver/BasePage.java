4af2b7556f0dadc71dfca20d1206d1f924efbe92
Victor - Thach Bui <thach.bui@orientsoftware.com>
2022-10-03 09:02:26 +0700

    f

--- src/main/java/com/qa/driver/BasePage.java
+++ src/main/java/com/qa/driver/BasePage.java
@@ -1,15 +1,18 @@
 package com.qa.driver;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.ITestContext;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
 
 public class BasePage {
     protected WebDriver driver;
 
     @Parameters ({"browser"})
     @BeforeMethod(alwaysRun = true)
-    public void setUp(@Optional ("chrome") String browser, ITestContext ctx) {
+    public void setUp(@Optional ("chrome") String browser) {
         // Init WebDriver
         BrowserDriverFactory factory = new BrowserDriverFactory(browser);
         driver = factory.createDriver();
@@ -21,9 +24,4 @@ public class BasePage {
         // Close browser
         driver.quit();
     }
-
-
-
-
-
 }
